1d819a714b366179465cc7f9e104b9356abc3b00
ekasriasih2201010245 <127836733+ekasriasih2201010245@users.noreply.github.com>
2023-03-14 16:56:05 +0800

    sesi1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan html sesi 1</title>
+</head>
+<body>
+    
+    <h3>belajar web programming<?="<sup>80</sup>"?></h3>
+
+    <script>
+        document.write("<h3>menggunakan script javascript</h3>");
+    </script>
+
+<?php
+     echo "<h3>menggunakan script</h3>";
+?>
+
+</body>
+</html>
